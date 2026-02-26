7fc048c07d254c22b47572f7176db506b6485498
P Smriti <smritip0806@gmail.com>
2026-02-26 22:00:23 +0530

    Fix Socket.IO CORS for production

--- backend/server.js
+++ backend/server.js
@@ -612,7 +612,8 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:3000",
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "https://peertutoria.vercel.app"
         ],
         methods: ["GET", "POST"],
         credentials: true
