ce3a8d46b246bc0dd31a0d125750bb7f23501c96
P Smriti <smritip0806@gmail.com>
2026-03-10 21:16:10 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const caPath = path.join(__dirname, "ca.pem");
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
 
